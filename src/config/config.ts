cff95f323b260573023ffc2423d504addd677dbd
HungTrinhIT <minhhung.it.work@gmail.com>
2024-08-30 00:40:36 +0700

    remove / from MONGO_URL

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,7 @@ dotenv.config();
 const MONGO_DB_USER = process.env.MONGO_DB_USER || '';
 const NODE_ENV = process.env.NODE_ENV || '';
 const MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD || '';
-const MONGO_URL = `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@atlascluster.apnjc.mongodb.net/tymex-marketplace/?retryWrites=true&w=majority&appName=AtlasCluster`;
+const MONGO_URL = `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@atlascluster.apnjc.mongodb.net/tymex-marketplace?retryWrites=true&w=majority&appName=AtlasCluster`;
 const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 5000;
 const MONGO_URL_LOCAL = `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@atlascluster.apnjc.mongodb.net/tymex-marketplace?retryWrites=true&w=majority&appName=AtlasCluster`;
 
